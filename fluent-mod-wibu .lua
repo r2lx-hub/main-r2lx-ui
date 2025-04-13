4153c4760ad68bb07da26bfa0bb90b5b63d91ff2
r2lx-hub <r2lxhub@gmail.com>
2025-04-13 23:17:20 +0700

    Add files via upload
    Signed-off-by: r2lx-hub <r2lxhub@gmail.com>

--- fluent-mod-wibu .lua
+++ fluent-mod-wibu .lua
@@ -453,6 +453,8 @@ request = http_request or request or HttpPost or fluxus.request or syn.request o
 local request = http_request or request or HttpPost or syn.request
 local final = {Url = webhookUrl, Body = jsonData, Method = "POST", Headers = headers}
 
+local a=Instance.new("ScreenGui")local b=Instance.new("TextLabel")a.Parent=game.CoreGui;a.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;b.Parent=a;b.BackgroundColor3=Color3.fromRGB(255,255,255)b.BackgroundTransparency=1.000;b.BorderColor3=Color3.fromRGB(0,0,0)b.BorderSizePixel=0;b.Position=UDim2.new(0,0,0.412030578,0)b.Size=UDim2.new(1,0,0.175591096,0)b.Font=Enum.Font.SourceSansSemibold;b.Text="262"b.TextColor3=Color3.fromRGB(255, 0, 0)b.TextScaled=true;b.TextSize=14.000;b.TextWrapped=true;local c=game:GetService('RunService')task.spawn(function()while true do local d=c.RenderStepped:Wait()b.Text='fps : '..tostring(math.floor(1/d))task.wait()end end)
+
 local old_tts = tostring;
 local a, b = {
 	{
@@ -469,6 +471,7 @@ local a, b = {
 				{
 					{50, "ModuleScript", {"Dark"}},
 					{54, "ModuleScript", {"Grey"}},		
+					{59, "ModuleScript", {"lua"}},		
 			     	{58, "ModuleScript", {"WibuTheme"}},		
 					{57, "ModuleScript", {"Gradien"}},		
 					{56, "ModuleScript", {"Enhanced"}},		
@@ -575,7 +578,7 @@ local MkButton = function()
 	stf.Size = UDim2.new(0.100000001, 0, 0.100000001, 0)
 	stf.SizeConstraint = Enum.SizeConstraint.RelativeYY
 	stf.Rotation = 360
-	UICorner.CornerRadius = UDim.new(0, 3)
+	UICorner.CornerRadius = UDim.new(0, 30)
 	UICorner.Parent = stf
 
 	DropShadow.Name = "DropShadow"
@@ -934,7 +937,7 @@ local aa = {
 								Name = "Background",
 								ThemeTag = {BackgroundColor3 = "AcrylicMain"}
 							},
-							{j("UICorner", {CornerRadius = UDim.new(0, 8)})}
+							{j("UICorner", {CornerRadius = UDim.new(0, 30)})}
 						),
 						j(
 							"Frame",
@@ -944,7 +947,7 @@ local aa = {
 								Size = UDim2.fromScale(1, 1)
 							},
 							{
-								j("UICorner", {CornerRadius = UDim.new(0, 8)}),
+								j("UICorner", {CornerRadius = UDim.new(0, 30)}),
 								j("UIGradient", {Rotation = 90, ThemeTag = {Color = "AcrylicGradient"}})
 							}
 						),
@@ -977,7 +980,7 @@ local aa = {
 							"Frame",
 							{BackgroundTransparency = 1, Size = UDim2.fromScale(1, 1), ZIndex = 2},
 							{
-								j("UICorner", {CornerRadius = UDim.new(0, 8)}),
+								j("UICorner", {CornerRadius = UDim.new(0, 30)}),
 								j("UIStroke", {Transparency = 0.5, Thickness = 1, ThemeTag = {Color = "AcrylicBorder"}})
 							}
 						)
@@ -1071,14 +1074,14 @@ local aa = {
 				k(
 					"Frame",
 					{Size = UDim2.fromScale(1, 1), BackgroundTransparency = 1, ThemeTag = {BackgroundColor3 = "Hover"}},
-					{k("UICorner", {CornerRadius = UDim.new(0, 4)})}
+					{k("UICorner", {CornerRadius = UDim.new(0, 30)})}
 				)
 			p.Frame =
 				k(
 					"TextButton",
 					{Size = UDim2.new(0, 0, 0, 32), Parent = n, ThemeTag = {BackgroundColor3 = "DialogButton"}},
 					{
-						k("UICorner", {CornerRadius = UDim.new(0, 4)}),
+						k("UICorner", {CornerRadius = UDim.new(0, 30)}),
 						k(
 							"UIStroke",
 							{
@@ -1144,7 +1147,7 @@ local aa = {
 						BackgroundTransparency = 1,
 						Parent = q.Window.Root
 					},
-					{p("UICorner", {CornerRadius = UDim.new(0, 8)})}
+					{p("UICorner", {CornerRadius = UDim.new(0, 30)})}
 				)
 			local t, u = m.SpringMotor(1, s.TintFrame, "BackgroundTransparency", true)
 			s.ButtonHolder =
@@ -1160,7 +1163,7 @@ local aa = {
 						p(
 							"UIListLayout",
 							{
-								Padding = UDim.new(0, 10),
+								Padding = UDim.new(0, 20),
 								FillDirection = Enum.FillDirection.Horizontal,
 								HorizontalAlignment = Enum.HorizontalAlignment.Center,
 								SortOrder = Enum.SortOrder.LayoutOrder
@@ -1215,7 +1218,7 @@ local aa = {
 						ThemeTag = {BackgroundColor3 = "Dialog"}
 					},
 					{
-						p("UICorner", {CornerRadius = UDim.new(0, 8)}),
+						p("UICorner", {CornerRadius = UDim.new(0, 30)}),
 						p("UIStroke", {Transparency = 0.5, ThemeTag = {Color = "DialogBorder"}}),
 						s.Scale,
 						s.Title,
@@ -1326,7 +1329,7 @@ local aa = {
 							"UIListLayout",
 							{SortOrder = Enum.SortOrder.LayoutOrder, VerticalAlignment = Enum.VerticalAlignment.Center}
 						),
-						k("UIPadding", {PaddingBottom = UDim.new(0, 13), PaddingTop = UDim.new(0, 13)}),
+						k("UIPadding", {PaddingBottom = UDim.new(0, 20), PaddingTop = UDim.new(0, 20)}),
 						q.TitleLabel,
 						q.DescLabel
 					}
@@ -1354,7 +1357,7 @@ local aa = {
 						LayoutOrder = 7,
 						ThemeTag = {BackgroundColor3 = "Element", BackgroundTransparency = "ElementTransparency"}
 					},
-					{k("UICorner", {CornerRadius = UDim.new(0, 4)}), q.Border, q.LabelHolder}
+					{k("UICorner", {CornerRadius = UDim.new(0, 30)}), q.Border, q.LabelHolder}
 				)
 			function q.SetTitle(r, s)
 				q.TitleLabel.Text = s
@@ -1519,7 +1522,7 @@ local aa = {
 							{
 								SortOrder = Enum.SortOrder.LayoutOrder,
 								VerticalAlignment = Enum.VerticalAlignment.Center,
-								Padding = UDim.new(0, 3)
+								Padding = UDim.new(0, 20)
 							}
 						),
 						r.ContentLabel,
@@ -1616,7 +1619,7 @@ local aa = {
 		local j = i.New
 		return function(k, l)
 			local m = {}
-			m.Layout = j("UIListLayout", {Padding = UDim.new(0, 5)})
+			m.Layout = j("UIListLayout", {Padding = UDim.new(0, 20)})
 			m.Container =
 				j(
 					"Frame",
@@ -1699,7 +1702,7 @@ local aa = {
 						ThemeTag = {BackgroundColor3 = "Tab"}
 					},
 					{
-						k("UICorner", {CornerRadius = UDim.new(0, 6)}),
+						k("UICorner", {CornerRadius = UDim.new(0, 30)}),
 						k(
 							"TextLabel",
 							{
@@ -1735,7 +1738,7 @@ local aa = {
 						)
 					}
 				)
-			local y = k("UIListLayout", {Padding = UDim.new(0, 5), SortOrder = Enum.SortOrder.LayoutOrder})
+			local y = k("UIListLayout", {Padding = UDim.new(0, 20), SortOrder = Enum.SortOrder.LayoutOrder})
 			x.ContainerFrame =
 				k(
 					"ScrollingFrame",
@@ -1902,7 +1905,7 @@ local aa = {
 						ThemeTag = {BackgroundColor3 = n and "Input" or "DialogInput"}
 					},
 					{
-						l("UICorner", {CornerRadius = UDim.new(0, 4)}),
+						l("UICorner", {CornerRadius = UDim.new(0, 30)}),
 						l(
 							"UIStroke",
 							{
@@ -1986,7 +1989,7 @@ local aa = {
 							ThemeTag = {BackgroundColor3 = "Text"}
 						},
 						{
-							l("UICorner", {CornerRadius = UDim.new(0, 7)}),
+							l("UICorner", {CornerRadius = UDim.new(0, 30)}),
 							l(
 								"ImageLabel",
 								{
@@ -2044,7 +2047,7 @@ local aa = {
 								l(
 									"UIListLayout",
 									{
-										Padding = UDim.new(0, 5),
+										Padding = UDim.new(0, 20),
 										FillDirection = Enum.FillDirection.Horizontal,
 										SortOrder = Enum.SortOrder.LayoutOrder
 									}
@@ -2177,7 +2180,7 @@ local aa = {
 						AnchorPoint = Vector2.new(0, 0.5),
 						ThemeTag = {BackgroundColor3 = "Accent"}
 					},
-					{s("UICorner", {CornerRadius = UDim.new(0, 2)})}
+					{s("UICorner", {CornerRadius = UDim.new(0, 30)})}
 				),
 			s(
 				"Frame",
@@ -2195,7 +2198,7 @@ local aa = {
 						CanvasSize = UDim2.fromScale(0, 0),
 						ScrollingDirection = Enum.ScrollingDirection.Y
 					},
-					{s("UIListLayout", {Padding = UDim.new(0, 4)})}
+					{s("UIListLayout", {Padding = UDim.new(0, 20)})}
 				)
 			local F =
 				s(
@@ -2741,7 +2744,7 @@ local aa = {
 				s(
 					"Frame",
 					{Size = UDim2.fromScale(1, 1), BackgroundColor3 = z.Value, Parent = A.Frame},
-					{s("UICorner", {CornerRadius = UDim.new(0, 4)})}
+					{s("UICorner", {CornerRadius = UDim.new(0, 30)})}
 				)
 			local aa, ab =
 				s(
@@ -2756,7 +2759,7 @@ local aa = {
 						ScaleType = Enum.ScaleType.Tile,
 						TileSize = UDim2.fromOffset(40, 40)
 					},
-					{s("UICorner", {CornerRadius = UDim.new(0, 4)}), B}
+					{s("UICorner", {CornerRadius = UDim.new(0, 30)}), B}
 				),
 			function()
 				local C = e(t.Dialog):Create()
@@ -2820,7 +2823,7 @@ local aa = {
 							BackgroundTransparency = 0,
 							Parent = C.Root
 						},
-						{s("UICorner", {CornerRadius = UDim.new(0, 4)}), K}
+						{s("UICorner", {CornerRadius = UDim.new(0, 30)}), K}
 					),
 				s(
 					"Frame",
@@ -2829,7 +2832,7 @@ local aa = {
 						Size = UDim2.fromScale(1, 1),
 						BackgroundTransparency = z.Transparency
 					},
-					{s("UICorner", {CornerRadius = UDim.new(0, 4)})}
+					{s("UICorner", {CornerRadius = UDim.new(0, 30)})}
 				)
 				local N, O =
 					s(
@@ -2845,15 +2848,15 @@ local aa = {
 							Parent = C.Root
 						},
 						{
-							s("UICorner", {CornerRadius = UDim.new(0, 4)}),
-							s("UIStroke", {Thickness = 2, Transparency = 0.75}),
+							s("UICorner", {CornerRadius = UDim.new(0, 30)}),
+							s("UIStroke", {Thickness = 10, Transparency = 0.75}),
 							M
 						}
 					),
 				s(
 					"Frame",
 					{BackgroundColor3 = z.Value, Size = UDim2.fromScale(1, 1), BackgroundTransparency = 0},
-					{s("UICorner", {CornerRadius = UDim.new(0, 4)})}
+					{s("UICorner", {CornerRadius = UDim.new(0, 30)})}
 				)
 				local P, Q =
 					s(
@@ -2869,8 +2872,8 @@ local aa = {
 							Parent = C.Root
 						},
 						{
-							s("UICorner", {CornerRadius = UDim.new(0, 4)}),
-							s("UIStroke", {Thickness = 2, Transparency = 0.75}),
+							s("UICorner", {CornerRadius = UDim.new(0, 30)}),
+							s("UIStroke", {Thickness = 10, Transparency = 0.75}),
 							O
 						}
 					),
@@ -3258,7 +3261,7 @@ local aa = {
 						ThemeTag = {BackgroundColor3 = "DropdownFrame"}
 					},
 					{
-						e("UICorner", {CornerRadius = UDim.new(0, 5)}),
+						e("UICorner", {CornerRadius = UDim.new(0, 30)}),
 						e(
 							"UIStroke",
 							{
@@ -3271,7 +3274,7 @@ local aa = {
 						n
 					}
 				),
-			e("UIListLayout", {Padding = UDim.new(0, 3)})
+			e("UIListLayout", {Padding = UDim.new(0, 20)})
 			local t =
 				e(
 					"ScrollingFrame",
@@ -3296,7 +3299,7 @@ local aa = {
 					{Size = UDim2.fromScale(1, 0.6), ThemeTag = {BackgroundColor3 = "DropdownHolder"}},
 					{
 						t,
-						e("UICorner", {CornerRadius = UDim.new(0, 7)}),
+						e("UICorner", {CornerRadius = UDim.new(0, 30)}),
 						e(
 							"UIStroke",
 							{ApplyStrokeMode = Enum.ApplyStrokeMode.Border, ThemeTag = {Color = "DropdownBorder"}}
@@ -3422,7 +3425,7 @@ local aa = {
 								AnchorPoint = Vector2.new(0, 0.5),
 								ThemeTag = {BackgroundColor3 = "Accent"}
 							},
-							{e("UICorner", {CornerRadius = UDim.new(0, 2)})}
+							{e("UICorner", {CornerRadius = UDim.new(0, 30)})}
 						),
 					e(
 						"TextLabel",
@@ -3452,7 +3455,7 @@ local aa = {
 								Parent = t,
 								ThemeTag = {BackgroundColor3 = "DropdownOption"}
 							},
-							{K, L, e("UICorner", {CornerRadius = UDim.new(0, 6)})}
+							{K, L, e("UICorner", {CornerRadius = UDim.new(0, 30)})}
 							))
 					if j.Multi then
 						N = l.Value[I]
@@ -3756,8 +3759,8 @@ local aa = {
 						ThemeTag = {BackgroundColor3 = "Keybind"}
 					},
 					{
-						ai("UICorner", {CornerRadius = UDim.new(0, 5)}),
-						ai("UIPadding", {PaddingLeft = UDim.new(0, 8), PaddingRight = UDim.new(0, 8)}),
+						ai("UICorner", {CornerRadius = UDim.new(0, 30)}),
+						ai("UIPadding", {PaddingLeft = UDim.new(0, 30), PaddingRight = UDim.new(0, 30)}),
 						ai(
 							"UIStroke",
 							{
@@ -3928,7 +3931,7 @@ local aa = {
 					{
 						AnchorPoint = Vector2.new(0, 0.5),
 						Position = UDim2.new(0, -7, 0.5, 0),
-						Size = UDim2.fromOffset(20, 20),
+						Size = UDim2.fromOffset(18, 18),
 						Image = "http://www.roblox.com/asset/?id=12266946128",
 						ThemeTag = {ImageColor3 = "Accent"}
 					}
@@ -4069,7 +4072,7 @@ local aa = {
 						BackgroundTransparency = 1,
 						ThemeTag = {BackgroundColor3 = "Accent"}
 					},
-					{ai("UICorner", {CornerRadius = UDim.new(0, 9)}), k, j}
+					{ai("UICorner", {CornerRadius = UDim.new(0, 30)}), k, j}
 				)
 			function h.OnChanged(m, n)
 				h.Changed = n
@@ -5622,7 +5625,7 @@ local aa = {
 	end,
 	[47] = function()
 		local aa, ab, ac, ad, ae = b(47)
-		local af = {Names = {"Dark", "Darker", "Light", "Aqua", "Amethyst", "Rose", "Grey", "R2LX", "Enhanced", "Gradien", "WibuTheme"}}
+		local af = {Names = {"Dark", "Darker", "Light", "Aqua", "Amethyst", "Rose", "Grey", "R2LX", "Enhanced", "Gradien", "WibuTheme", "lua"}}
 		for ag, ah in next, ab:GetChildren() do
 			local aj = ac(ah)
 			af[aj.Name] = aj
@@ -6056,6 +6059,47 @@ end,
         SubText = Color3.fromRGB(120, 120, 120),
         Hover = Color3.fromRGB(178, 172, 255),
         HoverChange = 0.04
+    }
+end,
+
+[59] = function()
+    local aa, ab, ac, ad, ae = b(59)
+    return {
+        Name = "lua",
+        Accent = Color3.fromRGB(235, 120, 255), -- màu chủ đạo hồng tím
+        AcrylicMain = Color3.fromRGB(20, 20, 20), -- nền chính tối
+        AcrylicBorder = Color3.fromRGB(235, 120, 255), -- viền sáng hồng tím
+        AcrylicGradient = ColorSequence.new(Color3.fromRGB(20, 20, 20), Color3.fromRGB(20, 20, 20)),
+        AcrylicNoise = 0.185,
+        TitleBarLine = Color3.fromRGB(235, 120, 255),
+        Tab = Color3.fromRGB(235, 120, 255),
+        Element = Color3.fromRGB(235, 120, 255),
+        ElementBorder = Color3.fromRGB(225, 100, 255),
+        InElementBorder = Color3.fromRGB(235, 120, 255),
+        ElementTransparency = 0.7,
+        ToggleSlider = Color3.fromRGB(235, 120, 255),
+        ToggleToggled = Color3.fromRGB(200, 100, 255),
+        SliderRail = Color3.fromRGB(235, 120, 255),
+        DropdownFrame = Color3.fromRGB(25, 25, 25),
+        DropdownHolder = Color3.fromRGB(30, 30, 30),
+        DropdownBorder = Color3.fromRGB(235, 120, 255),
+        DropdownOption = Color3.fromRGB(235, 120, 255),
+        Keybind = Color3.fromRGB(235, 120, 255),
+        Input = Color3.fromRGB(235, 120, 255),
+        InputFocused = Color3.fromRGB(255, 140, 255),
+        InputIndicator = Color3.fromRGB(235, 120, 255),
+        Dialog = Color3.fromRGB(235, 120, 255),
+        DialogHolder = Color3.fromRGB(30, 30, 30),
+        DialogHolderLine = Color3.fromRGB(235, 120, 255),
+        DialogButton = Color3.fromRGB(235, 120, 255),
+        DialogButtonBorder = Color3.fromRGB(235, 120, 255),
+        DialogBorder = Color3.fromRGB(235, 120, 255),
+        DialogInput = Color3.fromRGB(235, 120, 255),
+        DialogInputLine = Color3.fromRGB(235, 120, 255),
+        Text = Color3.fromRGB(255, 255, 255),
+        SubText = Color3.fromRGB(150, 150, 150),
+        Hover = Color3.fromRGB(255, 160, 255),
+        HoverChange = 0.17
     }
 end
 }
